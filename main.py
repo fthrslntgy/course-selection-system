f72d7ca833cb937307f474d2796d67ebf0ca2013
fthrslntgy <fatiharslantugay@gmail.com>
2022-08-14 02:53:09 +0300

    login checks completed

--- main.py
+++ main.py
@@ -1,140 +1,11 @@
 from flask import Flask, render_template, request, flash
-from sqlalchemy.dialects.postgresql import UUID
-from flask_sqlalchemy import SQLAlchemy
-import uuid
+import psycopg2
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:fatih1@localhost/dss'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.secret_key = 'secret string'
-db = SQLAlchemy(app)
-
-class User(db.Model):
-
-    __tablename__ = 'USER'
-    Username = db.Column(db.String(10), primary_key=True, nullable=False)
-    Password = db.Column(db.String(10), nullable=False)
-    Fname = db.Column(db.String(15), nullable=False)
-    Minit = db.Column(db.String(15), nullable=True)
-    Lname = db.Column(db.String(15), nullable=False)
-    Birthdate = db.Column(db.DateTime, nullable=True)
-    Address = db.Column(db.String(200), nullable=True)
-    PhoneNumber = db.Column(db.String(15), unique=True, nullable=True)
-    GroupType = db.Column(db.String(15), nullable=False)
-    FirstLogin = db.Column(db.Boolean, nullable=False)
-
-    def __init__(self, Username, Password, Fname, Minit, Lname, Birthdate, Address, PhoneNumber, GroupType, FirstLogin):
-        self.Username = Username
-        self.Password = Password
-        self.Fname = Fname       
-        self.Minit = Minit       
-        self.Lname = Lname
-        self.Birthdate = Birthdate
-        self.Address = Address
-        self.PhoneNumber = PhoneNumber
-        self.GroupType = GroupType
-        self.FirstLogin = FirstLogin
-
-class Notification(db.Model):
-
-    __tablename__ = 'NOTIFICATION'
-    N_Username = db.Column(db.String(10), primary_key=True, nullable=False)
-    NotificationID = db.Column(UUID(as_uuid=True), primary_key=True, unique=True, nullable=False)
-    Date = db.Column(db.DateTime, nullable=False)
-    Title = db.Column(db.String(20), nullable=False)
-    Message = db.Column(db.String(200), nullable=False)
-    Receipts = db.Column(db.Boolean, nullable=False)
-
-    def __init__(self, N_Username, Date, Title, Message, Receipts):
-        self.N_Username = N_Username
-        self.Date = Date       
-        self.Title = Title       
-        self.Message = Message
-        self.Receipts = Receipts
-
-class Admin(db.Model):
-
-    __tablename__ = 'ADMIN'
-    AD_Username = db.Column(db.String(10), primary_key=True, nullable=False)
-    AccessLevel = db.Column(db.Integer, nullable=False)
-
-    def __init__(self, AD_Username, AccessLevel):
-        self.AD_Username = AD_Username
-        self.AccessLevel = AccessLevel
-
-class Department(db.Model):
-
-    __tablename__ = 'DEPARTMENT'
-    Depcode = db.Column(db.String(3), primary_key=True, nullable=False)
-    Head = db.Column(db.String(10), unique=True, nullable=False)
-    CreditLimit = db.Column(db.Integer, nullable=False)
-
-    def __init__(self, Depcode, Head, CreditLimit):
-        self.Depcode = Depcode
-        self.Head = Head
-        self.CreditLimit = CreditLimit
-
-class Academician(db.Model):
-
-    __tablename__ = 'ACADEMICIAN'
-    A_Username = db.Column(db.String(10), primary_key=True, nullable=False)
-    A_DepCode = db.Column(db.String(3), nullable=False)
-    A_Degree = db.Column(db.String(10), nullable=False)
-
-    def __init__(self, A_Username, A_DepCode, A_Degree):
-        self.A_Username = A_Username
-        self.A_DepCode = A_DepCode
-        self.A_Degree = A_Degree
-
-class Student(db.Model):
-
-    __tablename__ = 'STUDENT'
-    S_Username = db.Column(db.String(10), primary_key=True, nullable=False)
-    S_DepCode = db.Column(db.String(3), nullable=False)
-    Advicer = db.Column(db.String(10), nullable=False)
-    StudentNumber = db.Column(db.Integer, unique=True, nullable=False)
-    S_Degree = db.Column(db.String(10), nullable=False)
-    Grade = db.Column(db.Integer, nullable=False)
-
-    def __init__(self, S_Username, S_DepCode, Advicer, StudentNumber, S_Degree, Grade):
-        self.S_Username = S_Username
-        self.S_DepCode = S_DepCode
-        self.Advicer = Advicer
-        self.StudentNumber = StudentNumber
-        self.S_Degree = S_Degree
-        self.Grade = Grade
-
-class Lecture(db.Model):
-
-    __tablename__ = 'LECTURE'
-    LectureCode = db.Column(db.Integer, primary_key=True, nullable=False)
-    L_DepCode = db.Column(db.String(3), primary_key=True, nullable=False)
-    Lecturer = db.Column(db.String(10), nullable=False)
-    Assistant = db.Column(db.String(10), unique=True, nullable=True)
-    Credit = db.Column(db.Integer, nullable=False)
-    Quota = db.Column(db.Integer, nullable=False)
-
-    def __init__(self, LectureCode, L_DepCode, Lecturer, Assistant, Credit, Quota):
-        self.LectureCode = LectureCode
-        self.L_DepCode = L_DepCode
-        self.Lecturer = Lecturer
-        self.Assistant = Assistant
-        self.Credit = Credit
-        self.Quota = Quota
-
-class Student_Lectures(db.Model):
-
-    __tablename__ = 'STUDENT_LECTURES'
-    SL_Username = db.Column(db.String(10), primary_key=True, nullable=False)
-    SL_Depcode = db.Column(db.String(3), primary_key=True, nullable=False)
-    SL_LectureCode = db.Column(db.Integer, primary_key=True, nullable=False)
-    Approved = db.Column(db.String(10), nullable=False)
-
-    def __init__(self, SL_Username, SL_Depcode, SL_LectureCode, Approved):
-        self.SL_Username = SL_Username
-        self.SL_Depcode = SL_Depcode
-        self.SL_LectureCode = SL_LectureCode
-        self.Approved = Approved
+def get_db_connection():
+    conn = psycopg2.connect(host='localhost', database='dss', user='postgres', password='fatih1')
+    return conn
+active_username = ""
 
 @app.route('/')  
 def home():    
@@ -144,19 +15,23 @@ def home():
 @app.route('/Login', methods=['GET', 'POST'])  
 def Login():        
     if(request.method == "POST"):
-        Username = request.form.get("username")
-        Password = request.form.get("password")
-        print(Username)
-        print(Password)
-        user = db.session.query(User.Username).filter(User.Username==Username).first()  
-        if user is not None:
-            db_pass = db.session.query(User.Password).filter(User.Username==Username).first()
-            db_pass = str(db_pass)           
-            if db_pass == Password:                     
-                return render_template('login.html', message="Hoşgeldiniz " + str(Username))    
-            else:               
-                return render_template('login.html', message="Hatalı şifre!")               
-        return render_template('login.html', message="Hatalı kullanıcı adı!")    
+        username = request.form.get("username")
+        password = request.form.get("password")
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT "Password" FROM "USER" WHERE username=\'' + username + '\';')
+        query = cur.fetchall()
+        cur.close()
+        conn.close()
+        if len(query) == 0:
+            return render_template('login.html', message=1)    
+        else: 
+            db_passwd = query[0][0]
+            if db_passwd == password:
+                return render_template('login.html', message=2)
+            else:
+                active_username = username
+                return render_template('login.html', message=0)    
 
 if __name__ == '__main__':
     app.run()
---
+++ models.py
@@ -0,0 +1,126 @@
+class User(db.Model):
+
+    __tablename__ = 'USER'
+    Username = db.Column(db.String(10), primary_key=True, nullable=False)
+    Password = db.Column(db.String(10), nullable=False)
+    Fname = db.Column(db.String(15), nullable=False)
+    Minit = db.Column(db.String(15), nullable=True)
+    Lname = db.Column(db.String(15), nullable=False)
+    Birthdate = db.Column(db.DateTime, nullable=True)
+    Address = db.Column(db.String(200), nullable=True)
+    PhoneNumber = db.Column(db.String(15), unique=True, nullable=True)
+    GroupType = db.Column(db.String(15), nullable=False)
+    FirstLogin = db.Column(db.Boolean, nullable=False)
+
+    def __init__(self, Username, Password, Fname, Minit, Lname, Birthdate, Address, PhoneNumber, GroupType, FirstLogin):
+        self.Username = Username
+        self.Password = Password
+        self.Fname = Fname       
+        self.Minit = Minit       
+        self.Lname = Lname
+        self.Birthdate = Birthdate
+        self.Address = Address
+        self.PhoneNumber = PhoneNumber
+        self.GroupType = GroupType
+        self.FirstLogin = FirstLogin
+
+class Notification(db.Model):
+
+    __tablename__ = 'NOTIFICATION'
+    N_Username = db.Column(db.String(10), primary_key=True, nullable=False)
+    NotificationID = db.Column(UUID(as_uuid=True), primary_key=True, unique=True, nullable=False)
+    Date = db.Column(db.DateTime, nullable=False)
+    Title = db.Column(db.String(20), nullable=False)
+    Message = db.Column(db.String(200), nullable=False)
+    Receipts = db.Column(db.Boolean, nullable=False)
+
+    def __init__(self, N_Username, Date, Title, Message, Receipts):
+        self.N_Username = N_Username
+        self.Date = Date       
+        self.Title = Title       
+        self.Message = Message
+        self.Receipts = Receipts
+
+class Admin(db.Model):
+
+    __tablename__ = 'ADMIN'
+    AD_Username = db.Column(db.String(10), primary_key=True, nullable=False)
+    AccessLevel = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, AD_Username, AccessLevel):
+        self.AD_Username = AD_Username
+        self.AccessLevel = AccessLevel
+
+class Department(db.Model):
+
+    __tablename__ = 'DEPARTMENT'
+    Depcode = db.Column(db.String(3), primary_key=True, nullable=False)
+    Head = db.Column(db.String(10), unique=True, nullable=False)
+    CreditLimit = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, Depcode, Head, CreditLimit):
+        self.Depcode = Depcode
+        self.Head = Head
+        self.CreditLimit = CreditLimit
+
+class Academician(db.Model):
+
+    __tablename__ = 'ACADEMICIAN'
+    A_Username = db.Column(db.String(10), primary_key=True, nullable=False)
+    A_DepCode = db.Column(db.String(3), nullable=False)
+    A_Degree = db.Column(db.String(10), nullable=False)
+
+    def __init__(self, A_Username, A_DepCode, A_Degree):
+        self.A_Username = A_Username
+        self.A_DepCode = A_DepCode
+        self.A_Degree = A_Degree
+
+class Student(db.Model):
+
+    __tablename__ = 'STUDENT'
+    S_Username = db.Column(db.String(10), primary_key=True, nullable=False)
+    S_DepCode = db.Column(db.String(3), nullable=False)
+    Advicer = db.Column(db.String(10), nullable=False)
+    StudentNumber = db.Column(db.Integer, unique=True, nullable=False)
+    S_Degree = db.Column(db.String(10), nullable=False)
+    Grade = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, S_Username, S_DepCode, Advicer, StudentNumber, S_Degree, Grade):
+        self.S_Username = S_Username
+        self.S_DepCode = S_DepCode
+        self.Advicer = Advicer
+        self.StudentNumber = StudentNumber
+        self.S_Degree = S_Degree
+        self.Grade = Grade
+
+class Lecture(db.Model):
+
+    __tablename__ = 'LECTURE'
+    LectureCode = db.Column(db.Integer, primary_key=True, nullable=False)
+    L_DepCode = db.Column(db.String(3), primary_key=True, nullable=False)
+    Lecturer = db.Column(db.String(10), nullable=False)
+    Assistant = db.Column(db.String(10), unique=True, nullable=True)
+    Credit = db.Column(db.Integer, nullable=False)
+    Quota = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, LectureCode, L_DepCode, Lecturer, Assistant, Credit, Quota):
+        self.LectureCode = LectureCode
+        self.L_DepCode = L_DepCode
+        self.Lecturer = Lecturer
+        self.Assistant = Assistant
+        self.Credit = Credit
+        self.Quota = Quota
+
+class Student_Lectures(db.Model):
+
+    __tablename__ = 'STUDENT_LECTURES'
+    SL_Username = db.Column(db.String(10), primary_key=True, nullable=False)
+    SL_Depcode = db.Column(db.String(3), primary_key=True, nullable=False)
+    SL_LectureCode = db.Column(db.Integer, primary_key=True, nullable=False)
+    Approved = db.Column(db.String(10), nullable=False)
+
+    def __init__(self, SL_Username, SL_Depcode, SL_LectureCode, Approved):
+        self.SL_Username = SL_Username
+        self.SL_Depcode = SL_Depcode
+        self.SL_LectureCode = SL_LectureCode
+        self.Approved = Approved
