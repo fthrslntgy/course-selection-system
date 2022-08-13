533c74338a6331c990427d03ea0e34d608a8b22e
MertCanGonen <mrtcngonen@gmail.com>
2022-08-14 02:53:40 +0300

    message check on login screen

---
+++ script/login.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    const msg = document.getElementById("message");
+    console.log(msg)
+};
+
+function inputCheck() {
+    const username = document.getElementById("username");
+    const password = document.getElementById("password");
+}
